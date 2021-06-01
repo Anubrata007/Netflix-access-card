e00b26be3d2af6232b4c33d89d7b4ce2f491d74f
Anubrata007 <anubrata995@gmail.com>
2021-06-01 19:45:22 +0530

    Initial commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp4056410.jpg",
+        title : "A Netflix Orignal Series",
+        sname : "DARK",
+        link : "https://www.netflix.com/in/title/80100172",
+    },
+
+    {
+        imgsrc : "https://m.media-amazon.com/images/M/MV5BNWU4NmY3MTMtMTBmMi00NjFjLTkwMmItYWZhZWUwNDg5M2ExXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg",
+        title : "A Netflix Orignal Series",
+        sname : "DEFFENDERS",
+        link : "https://www.netflix.com/in/title/80002566",
+    },
+
+    {
+        imgsrc : "https://mblogthumb-phinf.pstatic.net/MjAyMDA1MDhfMTA3/MDAxNTg4OTE4NjkyMTg2.UPV7fes17rHae4hcpiG514w5YE86FFl0H1biMjwjWBkg.HNJFreXpH0WullQsKmEOP4vtWBl7IvFulzl2jRYJ-twg.JPEG.jokun1214/Cap_2020-05-08_12-38-42-681.jpg?type=w800",
+        title : "A Netflix Orignal Series",
+        sname : "EXTRA CURRICULAR",
+        link : "https://www.netflix.com/in/title/80990668",
+    },
+
+    {
+        imgsrc : "https://i2.wp.com/thewordyhabitat.com/wp-content/uploads/2021/04/my-first-first-love-kdrama-poster.jpg?fit=983%2C1342&ssl=1",
+        title : "A Netflix Orignal Series",
+        sname : "MY FIRST-2 LOVE",
+        link : "https://www.netflix.com/in/title/81026700",
+    },
+
+    {
+        imgsrc : "https://i.redd.it/uucguz215uv21.jpg",
+        title : "A Netflix Orignal Series",
+        sname : "LUCIFER",
+        link : "https://www.netflix.com/in/title/80057918",
+    },
+];
+
+export default Sdata;
